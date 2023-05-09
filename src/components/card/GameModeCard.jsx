67c094a19a171d47ebfe7b62954e75e0f0063d0c
Sergey Fomenko <sfom_rabota@mail.ru>
2023-05-09 20:34:16 +0300

    fix: delet hook

--- src/components/card/GameModeCard.jsx
+++ src/components/card/GameModeCard.jsx
@@ -1,10 +1,7 @@
-import {useState} from 'react';
-
 import {gameModeInfo} from '../../data/gameModeInfo';
 import {GameModeCardItem} from './GameModeCardItem';
 
 const GameModeCard = () => {
-	const [gameMode, setGameMode] = useState(['easy', 'normal', 'hard', 'custom']);
 	return (
 		<div className="flex flex-wrap gap-5 justify-center">
 			{gameModeInfo.map((mode) => (
