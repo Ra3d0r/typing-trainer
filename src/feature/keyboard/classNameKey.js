f40a1f15bc2455e9c51371d70291a1766ad8e73f
Sergey Fomenko <sfom_rabota@mail.ru>
2023-05-20 14:10:45 +0300

    add return in switch case

--- src/feature/keyboard/classNameKey.js
+++ src/feature/keyboard/classNameKey.js
@@ -1,26 +1,19 @@
 export const classNameKey = (id, eventKey, keyTarget) => {
-	let className = '';
 	switch (true) {
 		case eventKey === keyTarget && id === eventKey:
-			className = 'target-typed';
-			break;
+			return 'target-typed';
 
 		case id === 'ShiftLeft' && id === eventKey:
 		case id === 'ShiftRight' && id === eventKey:
 		case id === 'Backspace' && id === eventKey:
-			className = 'special';
-			break;
+			return 'special';
 
 		case id === eventKey:
-			className = 'mis-typed';
-			break;
+			return 'mis-typed';
 		case id === keyTarget:
-			className = 'target';
-			break;
+			return 'target';
 
 		default:
-			className = 'untyped';
+			return 'untyped';
 	}
-
-	return className;
 };
