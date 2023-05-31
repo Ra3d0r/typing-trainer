168b1dcbc42e970eb10f2fc621355e25f2203db2
Sergey Fomenko <sfom_rabota@mail.ru>
2023-05-31 16:17:53 +0300

    feat: add icons for score

--- src/feature/score/components/item/Time.jsx
+++ src/feature/score/components/item/Time.jsx
@@ -1,13 +1,31 @@
+import {Indicator} from '../../../../components/score/Indicator';
 import {useTime} from '../../../../hooks/useTime';
+
+const Svg = () => {
+	return (
+		<svg
+			data-darkreader-inline-stroke
+			fill="none"
+			stroke="currentColor"
+			strokewidth="1.5"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+			aria-hidden="true"
+			className="w-6 h-6"
+		>
+			<path
+				strokelinecap="round"
+				strokelinejoin="round"
+				d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+			/>
+		</svg>
+	);
+};
 
 const Time = ({mode}) => {
 	const time = useTime(mode);
 
-	return (
-		<div>
-			<p>Time: {time}</p>
-		</div>
-	);
+	return <Indicator value={time} svg={<Svg />} text="Time" />;
 };
 
 export {Time};
--- src/feature/score/components/main/Score.jsx
+++ src/feature/score/components/main/Score.jsx
@@ -1,19 +1,31 @@
+import tw from 'twin.macro';
+
 import {useUpdateScore} from '../../../../hooks/useUpdateScore';
 import {Accuracy} from '../item/Accuracy';
 import {Time} from '../item/Time';
 import {TotalChars} from '../item/TotalChars';
 import {Typos} from '../item/Typos';
 
+const Container = ({children}) => {
+	return (
+		<div
+			css={tw`text-center text-blue-500 flex space-x-2 sm:space-x-10 justify-center flex-wrap gap-y-2`}
+		>
+			{children}
+		</div>
+	);
+};
+
 const Score = ({mode}) => {
 	useUpdateScore(mode);
 
 	return (
-		<div className="text-center text-blue-500">
+		<Container>
+			<Accuracy mode={mode} />
 			<Time mode={mode} />
 			<Typos mode={mode} />
-			<Accuracy mode={mode} />
 			<TotalChars mode={mode} />
-		</div>
+		</Container>
 	);
 };
 
