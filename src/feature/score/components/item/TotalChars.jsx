5495361cc6e314b5d0471fb6643d9c5c878227df
Sergey Fomenko <sfom_rabota@mail.ru>
2023-06-03 22:16:49 +0300

    fix: remove attributes

--- src/feature/score/components/item/TotalChars.jsx
+++ src/feature/score/components/item/TotalChars.jsx
@@ -9,16 +9,12 @@ const Svg = () => {
 			data-darkreader-inline-stroke
 			fill="none"
 			stroke="currentColor"
-			strokeWidth="1.5"
 			viewBox="0 0 24 24"
 			xmlns="http://www.w3.org/2000/svg"
 			aria-hidden="true"
 			className="w-6 h-6"
 		>
-			<path
-				strokeLinecap="round"
-				d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25"
-			/>
+			<path d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25" />
 		</svg>
 	);
 };
--- src/feature/score/components/item/Typos.jsx
+++ src/feature/score/components/item/Typos.jsx
@@ -9,13 +9,12 @@ const Svg = () => {
 			data-darkreader-inline-stroke
 			fill="none"
 			stroke="currentColor"
-			strokeWidth="1.5"
 			viewBox="0 0 24 24"
 			xmlns="http://www.w3.org/2000/svg"
 			aria-hidden="true"
 			className="w-6 h-6"
 		>
-			<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+			<path d="M6 18L18 6M6 6l12 12" />
 		</svg>
 	);
 };
