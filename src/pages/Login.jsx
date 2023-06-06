d16f15d2a76258821a387c5d71b6b87d738d2cd8
Sergey Fomenko <sfom_rabota@mail.ru>
2023-06-06 13:02:15 +0300

    feat: add react hook form and input

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,16 @@
+import {useForm} from 'react-hook-form';
+
+import Input from '../components/UI/Input';
+
 const Login = () => {
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: {errors},
+	} = useForm();
+	const onSubmit = (data) => console.log(data);
+
 	return (
 		<section className="bg-gray-50 dark:bg-gray-900">
 			<div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -18,37 +30,25 @@ const Login = () => {
 						<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
 							Sign in to your account
 						</h1>
-						<form className="space-y-4 md:space-y-6" action="#">
+						<form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit(onSubmit)}>
 							<div>
-								<label
-									htmlFor="email"
-									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-								>
-									Your email
-								</label>
-								<input
+								<Input
 									type="email"
 									name="email"
-									id="email"
-									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 									placeholder="name@company.com"
+									register={register}
 									required
+									label={'Email'}
 								/>
 							</div>
 							<div>
-								<label
-									htmlFor="password"
-									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-								>
-									Password
-								</label>
-								<input
+								<Input
 									type="password"
 									name="password"
-									id="password"
 									placeholder="••••••••"
-									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									register={register}
 									required
+									label={'Password'}
 								/>
 							</div>
 							<div className="flex items-center justify-between">
@@ -59,7 +59,6 @@ const Login = () => {
 											aria-describedby="remember"
 											type="checkbox"
 											className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-											required
 										/>
 									</div>
 									<div className="ml-3 text-sm">
