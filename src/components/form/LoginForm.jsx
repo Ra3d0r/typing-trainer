a1e9ccd9d45e85e62d02573ed685624099d08d46
Sergey Fomenko <sfom_rabota@mail.ru>
2023-06-07 14:24:23 +0300

    feat: add validation form Login

---
+++ src/components/form/LoginForm.jsx
@@ -0,0 +1,47 @@
+import Checkbox from '../UI/Checkbox';
+import Input from '../UI/Input';
+import LinkForm from '../UI/LinkForm';
+import Submit from '../UI/Submit';
+
+const LoginForm = ({onSubmit, register, errors, isValid}) => {
+	return (
+		<form className="space-y-4 md:space-y-6" action="#" onSubmit={onSubmit}>
+			<div>
+				<Input
+					type="email"
+					name="email"
+					placeholder="name@company.com"
+					register={register}
+					required={'Fill in this field'}
+					label={'Email'}
+				/>
+				{errors.email && (
+					<p className="text-sm text-red-500 dark:text-red-400 mt-2">{errors.email.message}</p>
+				)}
+			</div>
+			<div>
+				<Input
+					type="password"
+					name="password"
+					placeholder="••••••••"
+					register={register}
+					required={'Fill in this field'}
+					label={'Password'}
+				/>
+				{errors.password && (
+					<p className="text-sm text-red-500 dark:text-red-400 mt-2">{errors.password.message}</p>
+				)}
+			</div>
+			<div className="flex items-center justify-between">
+				<Checkbox id="remember" name="remember" register={register} label={'Remember me'} />
+				<LinkForm to={'/forgot-password'}>Forgot password?</LinkForm>
+			</div>
+			<Submit disabled={!isValid}>Sign in</Submit>
+			<p className="text-sm font-light text-gray-500 dark:text-gray-400">
+				Don’t have an account yet? <LinkForm to="/register">Sign up</LinkForm>
+			</p>
+		</form>
+	);
+};
+
+export default LoginForm;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,74 +1,7 @@
-import {useForm} from 'react-hook-form';
-
-import Checkbox from '../components/UI/Checkbox';
-import Input from '../components/UI/Input';
-import Logo from '../components/UI/Logo';
-import Submit from '../components/UI/Submit';
+import SignIn from '../feature/user/SignIn';
 
 const Login = () => {
-	const {
-		register,
-		handleSubmit,
-		watch,
-		formState: {errors},
-	} = useForm();
-	const onSubmit = (data) => console.log(data);
-
-	return (
-		<section className="bg-gray-50 dark:bg-gray-900">
-			<div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-				<Logo cls={`mb-4 text-gray-800 dark:text-white`}>Typing Trainer</Logo>
-				<div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-					<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-						<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-							Sign in to your account
-						</h1>
-						<form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit(onSubmit)}>
-							<div>
-								<Input
-									type="email"
-									name="email"
-									placeholder="name@company.com"
-									register={register}
-									required
-									label={'Email'}
-								/>
-							</div>
-							<div>
-								<Input
-									type="password"
-									name="password"
-									placeholder="••••••••"
-									register={register}
-									required
-									label={'Password'}
-								/>
-							</div>
-							<div className="flex items-center justify-between">
-								<Checkbox id="remember" name="remember" register={register} label={'Remember me'} />
-								<a
-									href="#"
-									className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
-								>
-									Forgot password?
-								</a>
-							</div>
-							<Submit>Sign in</Submit>
-							<p className="text-sm font-light text-gray-500 dark:text-gray-400">
-								Don’t have an account yet?{' '}
-								<a
-									href="#"
-									className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-								>
-									Sign up
-								</a>
-							</p>
-						</form>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+	return <SignIn />;
 };
 
 export default Login;
