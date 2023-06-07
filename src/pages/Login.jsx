8f67b2e913130b20728d09e460665fb4c753ba71
Sergey Fomenko <sfom_rabota@mail.ru>
2023-06-07 11:44:28 +0300

    fix: change color title for login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
 	return (
 		<section className="bg-gray-50 dark:bg-gray-900">
 			<div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-				<Logo cls={`mb-4`}>Typing Trainer</Logo>
+				<Logo cls={`mb-4 text-gray-800 dark:text-white`}>Typing Trainer</Logo>
 				<div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
 					<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
 						<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
