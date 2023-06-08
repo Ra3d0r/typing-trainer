2cfd52edfd0b8d2e9da9e2b118c35b3015dc208c
Sergey Fomenko <sfom_rabota@mail.ru>
2023-06-08 17:08:36 +0300

    feat: update checkbox

--- src/components/UI/Checkbox.jsx
+++ src/components/UI/Checkbox.jsx
@@ -1,4 +1,6 @@
-const Checkbox = ({id, name, register, required, label, ...other}) => {
+import {Link} from 'react-router-dom';
+
+const Checkbox = ({name, register, required, label, link, ...other}) => {
 	return (
 		<div className="flex items-start">
 			<div className="flex items-center h-5">
@@ -11,7 +13,16 @@ const Checkbox = ({id, name, register, required, label, ...other}) => {
 			</div>
 			<div className="ml-3 text-sm">
 				<label htmlFor={name} className="text-gray-500 dark:text-gray-300">
-					{label}
+					{label}{' '}
+					{link && (
+						<Link
+							className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+							to={link.to}
+						>
+							{' '}
+							{link.text}{' '}
+						</Link>
+					)}
 				</label>
 			</div>
 		</div>
