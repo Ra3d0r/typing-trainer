2b14728f57632329964b98e5bfcca43ad612b4f1
Sergey Fomenko <sfom_rabota@mail.ru>
2023-06-12 12:36:25 +0300

    fix: attribute reset form

--- src/components/form/ResetPasswordForm.jsx
+++ src/components/form/ResetPasswordForm.jsx
@@ -4,6 +4,9 @@ import EmailInput from './item/EmailInput';
 const ResetPasswordForm = ({onSubmit, register, errors, isValid}) => {
 	return (
 		<form className="space-y-4 md:space-y-6" onSubmit={onSubmit}>
+			<p className="text-sm font-light text-gray-500 dark:text-gray-400">
+				Enter your account email
+			</p>
 			<EmailInput register={register} errors={errors} />
 			<Submit disabled={!isValid}>Reset password</Submit>
 		</form>
