820aacc6887bff485503ce963b78f4cda871cd92
Sergey Fomenko <sfom_rabota@mail.ru>
2023-06-14 14:27:35 +0300

    feat: refacrtor create user and send email verificatin

--- src/feature/user/auth/createUser.js
+++ src/feature/user/auth/createUser.js
@@ -1,34 +1,39 @@
-import {createUserWithEmailAndPassword, updateProfile} from 'firebase/auth';
+import {createUserWithEmailAndPassword, sendEmailVerification, updateProfile} from 'firebase/auth';
 
 import {auth} from '../../../firebase';
 import {openToast} from '../../toast/toastSlice';
 import {setStatusUser, setUser} from '../userSlice';
 
-const createUser = ({email, password, login}, dispatch, navigate, reset) => {
-	dispatch(setStatusUser('loading'));
-	createUserWithEmailAndPassword(auth, email, password)
-		.then(() => {
-			updateProfile(auth.currentUser, {
-				displayName: login,
-			}).then(() => {
-				reset();
-				dispatch(
-					setUser({
-						login,
-						id: auth.currentUser.uid,
-						token: auth.currentUser.refreshToken,
-					}),
-				);
-				navigate('/account');
-			});
-		})
-		.catch((error) => {
-			const errorCode = error.code;
-			const errorMessage = error.message;
-			dispatch(setStatusUser('error'));
-			dispatch(openToast({message: errorMessage, type: 'error'}));
-			console.log(errorCode, errorMessage);
+const createUser = async ({email, password, login}, dispatch, navigate, reset) => {
+	try {
+		dispatch(setStatusUser('loading'));
+
+		await createUserWithEmailAndPassword(auth, email, password);
+
+		await sendEmailVerification(auth.currentUser);
+
+		dispatch(openToast({message: 'Verification email sent!', type: 'info'}));
+
+		await updateProfile(auth.currentUser, {
+			displayName: login,
 		});
+
+		reset();
+		dispatch(
+			setUser({
+				login,
+				id: auth.currentUser.uid,
+				token: auth.currentUser.refreshToken,
+			}),
+		);
+		navigate('/account');
+	} catch (error) {
+		const errorCode = error.code;
+		const errorMessage = error.message;
+		dispatch(setStatusUser('error'));
+		dispatch(openToast({message: errorMessage, type: 'error'}));
+		console.log(errorCode, errorMessage);
+	}
 };
 
 export default createUser;
