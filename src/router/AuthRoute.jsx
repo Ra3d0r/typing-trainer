bc00912d7ff994c9ad1d5d563b81b79eaafd2011
Sergey Fomenko <sfom_rabota@mail.ru>
2023-06-19 15:34:34 +0300

    feat: loading sreen if user loading

--- src/router/AuthRoute.jsx
+++ src/router/AuthRoute.jsx
@@ -1,10 +1,17 @@
+import {useAuthState} from 'react-firebase-hooks/auth';
 import {useSelector} from 'react-redux';
 import {Navigate, Outlet} from 'react-router-dom';
 
+import LoadingFullScreen from '../components/compose/LoadingFullScreen';
 import {selectUserAuth} from '../feature/user/userSlice';
+import {auth} from '../firebase';
 
 const AuthRoute = () => {
 	const isAuthenticated = useSelector(selectUserAuth);
+	const [_, loading] = useAuthState(auth);
+	if (loading) {
+		return <LoadingFullScreen />;
+	}
 	return isAuthenticated ? <Navigate to="/account" /> : <Outlet />;
 };
 
--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,10 +1,17 @@
+import {useAuthState} from 'react-firebase-hooks/auth';
 import {useSelector} from 'react-redux';
 import {Navigate, Outlet} from 'react-router-dom';
 
+import LoadingFullScreen from '../components/compose/LoadingFullScreen';
 import {selectUserAuth} from '../feature/user/userSlice';
+import {auth} from '../firebase';
 
 const PrivateRoute = () => {
 	const isAuthenticated = useSelector(selectUserAuth);
+	const [_, loading] = useAuthState(auth);
+	if (loading) {
+		return <LoadingFullScreen />;
+	}
 	return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
