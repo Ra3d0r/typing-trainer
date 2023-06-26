80a1eb5a0abf33d133b697e8ca8519916ac48dac
Sergey Fomenko <sfom_rabota@mail.ru>
2023-06-26 18:22:27 +0300

    feat: firebase config ts

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+
+const firebaseConfig = {
+	apiKey: process.env['FIREBASE_API_KEY'] as string,
+	authDomain: process.env['FIREBASE_AUTH_DOMAIN'] as string,
+	projectId: process.env['FIREBASE_PROJECT_ID'] as string,
+	storageBucket: process.env['FIREBASE_STORAGE_BUCKET'] as string,
+	messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'] as string,
+	appId: process.env['FIREBASE_APP_ID'] as string,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
