66728cd55ddc6e452fd6a629f2dea88ae251605b
Sergey Fomenko <sfom_rabota@mail.ru>
2023-07-05 18:08:01 +0300

    feat: types for pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import tw from 'twin.macro';
 import GameModeCard from '../components/card/GameModeCard';
 import Toast from '../feature/toast/Toast';
 
-const Title = ({children}) => {
+const Title = ({children}: {children: React.ReactNode}) => {
 	return (
 		<h1
 			css={tw`mb-20 mt-20 text-center text-4xl font-extrabold sm:leading-3 md:leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white`}
@@ -13,7 +13,7 @@ const Title = ({children}) => {
 	);
 };
 
-const Mark = ({children}) => {
+const Mark = ({children}: {children: React.ReactNode}) => {
 	return <mark css={tw`px-2 text-white bg-blue-600 rounded dark:bg-blue-500`}>{children}</mark>;
 };
 
