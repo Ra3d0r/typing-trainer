516dea40505c9b5552b260c029a8a60b53138c53
Sergey Fomenko <sfom_rabota@mail.ru>
2023-07-19 14:16:42 +0300

    feat: add sizes for button

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,21 +1,32 @@
-import tw from 'twin.macro';
+import {IPropsButton, ISize} from './types/typesButton';
 
-import {IPropsButton} from './types/typesButton';
-
-const alternative = tw`
-py-3 px-5  text-sm font-medium text-gray-900 
+const alternative = `
+font-medium text-gray-900 
 focus:outline-none bg-white rounded-lg border border-gray-200 
 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 
 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 
 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700
 `;
 
-const defaultButton = tw`text-white bg-blue-700 hover:bg-blue-800 focus:ring-2 focus:ring-blue-300 font-medium 
-rounded-lg text-sm px-5 py-3  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800`;
+const defaultButton = `text-white bg-blue-700 hover:bg-blue-800 focus:ring-2 focus:ring-blue-300 font-medium 
+rounded-lg dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800`;
 
-const Button = ({children, onClick, isAlternative = false}: IPropsButton) => {
+const stylesSize: ISize = {
+	small: `px-2.5 py-1.5 text-xs`,
+	medium: `px-5 py-3 text-sm`,
+	large: `px-4 py-2 text-base`,
+	fullMedium: `w-full px-5 py-3 text-sm`,
+};
+
+const styleButton = (size: keyof ISize, isAlternative: boolean) => {
+	return isAlternative
+		? `${stylesSize[size]} ${alternative}`
+		: `${stylesSize[size]} ${defaultButton}`;
+};
+
+const Button = ({children, onClick, isAlternative = false, size = 'medium'}: IPropsButton) => {
 	return (
-		<button type="button" css={[defaultButton, isAlternative && alternative]} onClick={onClick}>
+		<button type="button" className={styleButton(size, isAlternative)} onClick={onClick}>
 			{children}
 		</button>
 	);
