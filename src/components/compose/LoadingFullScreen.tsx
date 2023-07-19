21f1f81558d4945730d94f917b1c445ae5f81a07
Sergey Fomenko <sfom_rabota@mail.ru>
2023-07-19 14:40:10 +0300

    fix: spinner loading on center for sidebar page

--- src/components/compose/LoadingFullScreen.tsx
+++ src/components/compose/LoadingFullScreen.tsx
@@ -2,8 +2,10 @@ import Loading from '@components/status/Loading';
 
 const LoadingFullScreen = () => {
 	return (
-		<div className="flex justify-center items-center h-screen">
-			<Loading />
+		<div className="fixed top-0 left-0 w-screen h-screen">
+			<div className="flex justify-center items-center h-screen">
+				<Loading />
+			</div>
 		</div>
 	);
 };
