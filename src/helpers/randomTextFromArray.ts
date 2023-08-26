d438dc05f18bdfc705a0ca8b6739fae66b1f5e85
Sergey Fomenko <sfom_rabota@mail.ru>
2023-08-26 14:53:57 +0300

    feat: add new throw Error

--- src/helpers/randomTextFromArray.ts
+++ src/helpers/randomTextFromArray.ts
@@ -19,13 +19,35 @@ const randomTextFromArray: typeRandomTextFromArray = (array, mode, textKey) => {
 
 	if (typeof text === 'object' && typeof key === 'string') {
 		const result: unknown = findValue(text, key);
+
 		if (typeof result === 'string') {
 			return result;
 		}
+
+		if (result === null) {
+			throw new Error('Value not found by key');
+		}
+
 		throw new Error('Text for typing must been string');
 	}
 
-	throw new Error('The text by key written in textKey in config.ts is not found');
+	if (Array.isArray(text)) {
+		throw new Error(
+			'The values in the array passed to the function argument must be a string or an object',
+		);
+	}
+
+	if (key === null && typeof text === 'object') {
+		throw new Error('Specify the key to the object');
+	}
+
+	if (text === undefined) {
+		throw new Error(
+			'The value at a random index in the array passed to the function is not received',
+		);
+	}
+
+	throw new Error('There was an unforeseen error');
 };
 
 export default randomTextFromArray;
