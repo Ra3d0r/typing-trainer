e046f49150e4a98a0cd2580ac9ab0285c99b7111
Sergey Fomenko <sfom_rabota@mail.ru>
2023-08-27 22:35:40 +0300

    feat: first test

---
+++ src/helpers/__tests__/millisecondsToDigitTime.test.ts
@@ -0,0 +1,13 @@
+import millisecondsToDigitTime from '../millisecondsToDigitTime';
+
+describe('millisecondsToDigitTime', () => {
+	it('should return "00:01"', () => {
+		expect(millisecondsToDigitTime(1000)).toEqual('00:01');
+	});
+	it('should return "00:00"', () => {
+		expect(millisecondsToDigitTime(100)).toEqual('00:00');
+	});
+	it('should return "00:12"', () => {
+		expect(millisecondsToDigitTime(12500)).toEqual('00:12');
+	});
+});
