012cf0c66e5f9221776346c677fa7483428257bb
Sergey Fomenko <sfom_rabota@mail.ru>
2023-08-27 23:02:12 +0300

    fix: used any method for compare values

--- src/helpers/__tests__/millisecondsToDigitTime.test.ts
+++ src/helpers/__tests__/millisecondsToDigitTime.test.ts
@@ -2,18 +2,18 @@ import millisecondsToDigitTime from '../millisecondsToDigitTime';
 
 describe('millisecondsToDigitTime', () => {
 	it('should return "00:01"', () => {
-		expect(millisecondsToDigitTime(1000)).toEqual('00:01');
+		expect(millisecondsToDigitTime(1000)).toBe('00:01');
 	});
 	it('should return "00:00"', () => {
-		expect(millisecondsToDigitTime(100)).toEqual('00:00');
+		expect(millisecondsToDigitTime(100)).toBe('00:00');
 	});
 	it('should return "00:12"', () => {
-		expect(millisecondsToDigitTime(12500)).toEqual('00:12');
+		expect(millisecondsToDigitTime(12500)).toBe('00:12');
 	});
 	it('should return "00:00"', () => {
-		expect(millisecondsToDigitTime(-5000)).toEqual('00:00');
+		expect(millisecondsToDigitTime(-5000)).toBe('00:00');
 	});
 	it('should return "00:00"', () => {
-		expect(millisecondsToDigitTime(0)).toEqual('00:00');
+		expect(millisecondsToDigitTime(0)).toBe('00:00');
 	});
 });
