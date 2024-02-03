c1cd8a003c87c19f1f05ff21f05b98d97a142c9d
Sergey Fomenko <sfom_rabota@mail.ru>
2024-02-03 11:39:55 +0300

    fix: up key working correct

--- src/helpers/types/typeHandleKeyUp.ts
+++ src/helpers/types/typeHandleKeyUp.ts
@@ -1,23 +1,7 @@
-import {TFunction} from 'i18next';
-
-import {typeLang, typeModeUnion} from '@types';
-
-import {AppDispatch} from '@store/store';
-
 interface params {
 	event: KeyboardEvent;
 	setIsShiftPressed: React.Dispatch<React.SetStateAction<boolean>>;
 	setEventKeyCode: React.Dispatch<React.SetStateAction<string>>;
-	target: string | undefined;
-	dispatch: AppDispatch;
-	currentText: string[];
-	currentTextIndex: number;
-	mode: typeModeUnion;
-	allText: string[] | Record<string, unknown>[];
-	isAuth: boolean;
-	errorsIndex: Record<number, 'error'>;
-	lang: typeLang;
-	t: TFunction<'translation'>;
 }
 
 export type typeHandleKeyUp = (arg: params) => void;
